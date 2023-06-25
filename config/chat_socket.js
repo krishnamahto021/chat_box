9e9bbe7b3a3974c71566097de79c7de1702b4a41
KRISHNA MAHATO <mkrishna98212@gmail.com>
2023-06-25 11:30:49 +0530

    fix the bug of chat box

--- config/chat_socket.js
+++ config/chat_socket.js
@@ -1,21 +1,22 @@
 module.exports.chatSockets = function(socketServer){
     let io = require('socket.io')(socketServer,{cors:{origin:'*'}});
-
+  
     io.sockets.on('connection',function(socket){
-        console.log('socket connected!');
-
-        socket.on('new-user-joined', data => {
-            socket.join(data.chatRoom);
-            socket.broadcast.to(data.chatRoom).emit('user_joined', data);
-        });
-
-        socket.on('send_message',function(data){
-            io.to(data.chatRoom).emit('recieve_message',data);
-
-        })
-
-        socket.on('disconnect',function(data){
-            socket.broadcast.to('chatBox').emit('leave',data);
-        })
-    })
-}
+      console.log('socket connected!');
+  
+      socket.on('new-user-joined', data => {
+        socket.join(data.chatRoom);
+        socket.broadcast.to(data.chatRoom).emit('user_joined', data);
+        socket.userName = data.user_name;
+      });
+  
+      socket.on('send_message', function(data){
+        io.to(data.chatRoom).emit('receive_message', data);
+      });
+  
+      socket.on('disconnect', function() {
+        socket.broadcast.emit('leave',{user_name:socket.userName});
+      });
+    });
+  };
+  
